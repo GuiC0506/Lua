1199b0dfa96a2b999e3bbc639186e6ffa3a697bb
Guilherme China <echina725@gmail.com>
2024-03-11 19:14:57 -0300

    variables and scope

--- main.lua
+++ main.lua
@@ -1,2 +1,34 @@
 -- a comment
+
+--[[
+    A multiline comment
+-- ]]
+
+Global_variable = "global"
+
+-- printing text
 print("Hello world")
+print("Hello " .. "world")
+
+local name = "churros"
+local anything = nil
+local number = 10
+
+local multiline_string = [[
+    A long
+    long
+    long
+    long
+    text
+]]
+
+print(name)
+name = false
+print(name)
+
+print(anything)
+print(number)
+print(multiline_string)
+print(nil==false)
+print(""==true)
+print(type(name))
